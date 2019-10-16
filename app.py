b15da900696606981f31c075b36a8ce0a8b69227
alexxsanya <alexxsanya@gmail.com>
2019-10-16 14:16:55 +0300

    add missing fields to Venue Model

--- app.py
+++ app.py
@@ -39,6 +39,10 @@ class Venue(db.Model):
   phone = db.Column(db.String(120))
   image_link = db.Column(db.String(500))
   facebook_link = db.Column(db.String(120))
+  genre = db.Column(db.String(120))
+  website = db.Column(db.String(120))
+  seeking_talent = db.Column(db.Boolean)
+  seeking_description = db.Column(db.String(500))
   # TODO: implement any missing fields, as a database migration using Flask-Migrate - Done
 
 class Artist(db.Model):
