10f4b995b6db72e58c66803a7714bd73315e7a5e
alexxsanya <alexxsanya@gmail.com>
2019-10-16 14:29:05 +0300

    update artist model and table

--- app.py
+++ app.py
@@ -56,6 +56,9 @@ class Artist(db.Model):
   genres = db.Column(db.String(120))
   image_link = db.Column(db.String(500))
   facebook_link = db.Column(db.String(120))
+  website = db.Column(db.String(120))
+  seeking_venue =  db.Column(db.Boolean)
+  seeking_description = db.Column(db.String(500))
   # TODO: implement any missing fields, as a database migration using Flask-Migrate - Done
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
