b4748fc2ed30a24239e6f59099acfb2565a293a6
alexxsanya <alexxsanya@gmail.com>
2019-10-18 18:39:56 +0300

    remove unnecessary comments

--- app.py
+++ app.py
@@ -231,9 +231,6 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
     try:
         search_term = request.form.get('search_term', '')
         artists = Artist.query.order_by(Artist.id).filter(
