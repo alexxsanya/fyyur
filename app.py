db921e22e5b7d42d251991f9e74976f8dc19566d
alexxsanya <alexxsanya@gmail.com>
2019-10-18 18:24:40 +0300

    update get all artists api

--- app.py
+++ app.py
@@ -219,17 +219,13 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    artists = Artist.query.order_by(Artist.id).all()
+    data = [
+      {
+        'id': artist.id,
+        'name': artist.name
+      } 
+    for artist in artists]
     return render_template('pages/artists.html', artists=data)
 
 
